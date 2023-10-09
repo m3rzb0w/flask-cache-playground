315f47dea2cc805d7c2518d615ac0a8b1fd74fc1
m3rzb0w <80rN70c0D3@protonmail.com>
2023-10-09 21:32:40 +0000

    caching var

--- app.py
+++ app.py
@@ -35,12 +35,39 @@ class Catfact(Resource):
         response.raise_for_status()
         return loads(response.text)
 
+class Name(Resource):
+    NAME_API_URL = "https://api.genderize.io?name="
+    NAME_WHITELIST = {"erlich" : True, "richard" : True}
+    def get(self, name):
+        if self.NAME_WHITELIST.get(name):
+            name_cache = cache.get(f"name_{name}")
+            if name_cache is None:
+                try:
+                    response = self.fetch_name(name)
+                    cache.set(f"name_{name}", response, timeout=10)
+                    return response, 200
+                except requests.exceptions.RequestException as e:
+                    return {"error": f"Fetching name - {e}"}, 404
+            else:
+                return name_cache, 200
+        else:
+            return {"error": f"Fetching name"}, 404
+    
+    def fetch_name(self, name):
+        url = f"{self.NAME_API_URL}{name}"
+        headers = {}
+        payload = {}
+        response = requests.get(url, headers=headers, data=payload)
+        response.raise_for_status()
+        return loads(response.text)
+
 class Ping(Resource):
     def get(self):
         current_datetime = datetime.now()
         return {"ping" : f"{current_datetime}"}, 200
 
 api.add_resource(Catfact, '/catfact')
+api.add_resource(Name, '/name/<string:name>')
 api.add_resource(Ping, '/ping')
 
 if __name__ == '__main__':
